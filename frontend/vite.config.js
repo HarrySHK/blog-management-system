2c91a6717b4a6445dd70a3603f1590c77238fefe
harryshk <harisnadeemshk@gmail.com>
2025-11-02 16:46:29 +0500

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://blog-management-system-yhkg.onrender.com/',
+        target: 'https://blog-management-system-yhkg.onrender.com',
         changeOrigin: true,
       },
     },
